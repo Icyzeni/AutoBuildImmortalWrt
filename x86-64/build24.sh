10f161cad92a7fafa63f5e0d2a49ab938cfeb6ed
IcyZen <icyzen@foxmail.com>
2025-08-18 16:09:53 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -58,6 +58,8 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
+# easytier
+PACKAGES="$PACKAGES luci-app-easytier	luci-i18n-easytier-zh-cn"
 # ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
